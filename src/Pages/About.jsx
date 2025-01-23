3adff3621e64c376a69e4bf55a0a9aa3cc2424fa
aswer <aswert092@gmail.com>
2025-01-23 17:26:36 +0300

    done

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -218,27 +218,6 @@ const AboutPage = () => {
               who is interested in developing Python, C# and Kotlin applications.
               Focuses on quality and "wow effect" in his projects.
             </p>
-
-            <div className="flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-4 lg:px-0 w-full">
-              <a href="https://docs.google.com/document/d/1cq3JcMZszvwZ2Kh1KvMFFTyietfoapwsxSPEKn8HxGY/edit?usp=sharing" className="w-full lg:w-auto">
-              <button 
-                data-aos="fade-up"
-                data-aos-duration="800"
-                className="w-full lg:w-auto sm:px-6 py-2 sm:py-3 rounded-lg bg-gradient-to-r from-[#6366f1] to-[#a855f7] text-white font-medium transition-all duration-300 hover:scale-105 flex items-center justify-center lg:justify-start gap-2 shadow-lg hover:shadow-xl animate-bounce-slow"
-              >
-                <FileText className="w-4 h-4 sm:w-5 sm:h-5" /> Download CV
-              </button>
-              </a>
-              <a href="#Portofolio" className="w-full lg:w-auto">
-              <button 
-                data-aos="fade-up"
-                data-aos-duration="1000"
-                className="w-full lg:w-auto sm:px-6 py-2 sm:py-3 rounded-lg border border-[#a855f7]/50 text-[#a855f7] font-medium transition-all duration-300 hover:scale-105 flex items-center justify-center lg:justify-start gap-2 hover:bg-[#a855f7]/10 animate-bounce-slow delay-200"
-              >
-                <Code className="w-4 h-4 sm:w-5 sm:h-5" /> View Projects
-              </button>
-              </a>
-            </div>
           </div>
 
           <ProfileImage />
